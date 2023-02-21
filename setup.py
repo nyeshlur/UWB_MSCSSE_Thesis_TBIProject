762900ef5e9c4be6dc9bcf29b39271f9103972f9
Nayana Yeshlur <nyeshlur@otachi.uwb.edu>
2023-02-21 14:19:34 -0800

    revised

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='TBI Diagnosis',
+    version='1.0',
+    description='Diagnosis of TBI using Pulsatility Ultrasound',
+    packages=['src'],
+    install_requires=[
+        'numpy',
+        'h5py',
+        'tensorflow',
+        'tensorboard',
+        'Jupyter',
+        'keras',
+        'matplotlib',
+        'opencv-python',
+        'scikit-learn',
+        'scipy'
+    ]
+)
