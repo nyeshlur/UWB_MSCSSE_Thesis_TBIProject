af08d2b5ba554cefbc59e6260b7a3c5f912772b4
Nayana Yeshlur <nyeshlur@otachi.uwb.edu>
2023-05-11 07:37:12 -0700

    revised

--- FullyAverage.m
+++ FullyAverage.m
@@ -1,46 +1,28 @@
-fileName = '/DATA/TBI/HealthyCondensed/DoD301/DoD301RA1.mat';
-RA1_301 = load(fileName);
+fileName = '/DATA/TBI/TBICondensed/DoD001/DoD001RA1.mat';
+RA1_001 = load(fileName);
 
-fileName = '/DATA/TBI/HealthyCondensed/DoD302/DoD302RA1.mat';
-RA1_302 = load(fileName);
+fileName = '/DATA/TBI/TBICondensed/DoD001/DoD001RA2.mat';
+RA2_001 = load(fileName);
 
-fileName = '/DATA/TBI/HealthyCondensed/DoD303/DoD303RA1.mat';
-RA1_303 = load(fileName);
+fileName = '/DATA/TBI/TBICondensed/DoD001/DoD001RA3.mat';
+RA3_001 = load(fileName);
 
-fileName = '/DATA/TBI/HealthyCondensed/DoD304/DoD304RA1.mat';
-RA1_304 = load(fileName);
+fileName = '/DATA/TBI/TBICondensed/DoD001/DoD001RA4.mat';
+RA4_001 = load(fileName);
 
-fileName = '/DATA/TBI/HealthyCondensed/DoD305/DoD305RA1.mat';
-RA1_305 = load(fileName);
-
-fileName = '/DATA/TBI/HealthyCondensed/DoD306/DoD306RA1.mat';
-RA1_306 = load(fileName);
-
-fileName = '/DATA/TBI/HealthyCondensed/DoD307/DoD307RA1.mat';
-RA1_307 = load(fileName);
-
-fileName = '/DATA/TBI/HealthyCondensed/DoD308/DoD308RA1.mat';
-RA1_308 = load(fileName);
-
-fileName = '/DATA/TBI/HealthyCondensed/DoD309/DoD309RA1.mat';
-RA1_309 = load(fileName);
-
-fileName = '/DATA/TBI/HealthyCondensed/DoD311/DoD311RA1.mat';
-RA1_311 = load(fileName);
-
-fileName = '/DATA/TBI/HealthyCondensed/DoD312/DoD312RA1.mat';
-RA1_312 = load(fileName);
-
-fileName = '/DATA/TBI/HealthyCondensed/DoD313/DoD313RA1.mat';
-RA1_313 = load(fileName);
+fileName = '/DATA/TBI/TBICondensed/DoD001/DoD001RA5.mat';
+RA5_001 = load(fileName);
 
 
-sum = RA1_301.single_cycle + RA1_302.single_cycle + RA1_303.single_cycle + RA1_304.single_cycle + RA1_305.single_cycle + RA1_306.single_cycle ...
-+ RA1_307.single_cycle + RA1_308.single_cycle + RA1_309.single_cycle + RA1_311.single_cycle + RA1_312.single_cycle + RA1_313.single_cycle;
+
+
+sum = RA1_001.single_cycle + RA2_001.single_cycle + RA3_001.single_cycle + RA4_001.single_cycle + RA5_001.single_cycle;
 
 sum = sum / 10;
 
 axis = linspace(1, 30, 30);
+
+id_angle = "DoD001RA";
 
 fig = figure(1);
 subplot(121);
@@ -49,10 +31,10 @@ xticks(0:1:30);
 grid on;
 xlabel("frames");
 ylabel("displacement");
-myTitle = "Averaged Displacement - " + "Healthy RA1";
+myTitle = "Averaged Displacement - " + id_angle;
 title(myTitle);
-figName = "HealthyRA1" + ".pdf";
-
+figName = id_angle + ".pdf";
 saveas(fig, figName)
 
-save("HealthyRA1.mat", "sum")
+fName = id_angle + ".mat";
+save(fName, "sum")
